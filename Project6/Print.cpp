2dbf0fd67fa8e7213486813383122cf95a9d83ae
Alex <Alex@DESKTOP-CEQG6CC>
2019-11-04 23:33:38 +0200

    Print_Pointers

--- Project6/Print.cpp
+++ Project6/Print.cpp
@@ -16,14 +16,19 @@ void Print(T Arr[], const int n)
 template<typename T>
 void Print(T Arr[ROWS][COLS])
 {
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
+	for (int i = 0; i < ROWS * COLS; i++)
+	{		
+		if (i % COLS == 0)
 		{
-			std::cout << Arr[i][j] << "\t";
+			std::cout << std::endl;
+			std::cout << *(&Arr[0][0] + i) << "\t";
 		}
-		std::cout <<std::endl;
+		else
+
+		{
+			std::cout << *(&Arr[0][0] + i) << "\t";
+		}
+		
 	}
-
-
+	std::cout << std::endl;
 }
